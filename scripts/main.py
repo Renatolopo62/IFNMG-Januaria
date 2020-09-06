05ca524960cd009e356fed8ac4af8655b4ffdc2c
Renato Lopo <renatolopo62@gmail.com>
2020-09-06 17:01:06 -0300

    add verificações

--- scripts/main.py
+++ scripts/main.py
@@ -1,6 +1,16 @@
 import getters as get
+import verificacoes as verifica
+import pandas as pd
 
-cod_cursos = get.getCodCurso()
+#cod_cursos = get.getCodCurso()
 
 #get.getMatriculas(cod_cursos, '2009')
-get.getNomeCurso(cod_cursos)
+#get.getNomeCurso(cod_cursos)
+#get.getDadosPadronizados()
+
+#print(get.LIST_COLUMNS)
+#529
+
+df = pd.read_csv('../dados/dados_padronizados_matriculas_januaria_2009_2018_com_id.csv')
+#verifica.contIdRepetido(df)
+verifica.matriculas_repetidas(df)
